48a857b33e9b8acfc106e7799923d43bc5706b30
OldFresco <hinsonkojo@gmail.com>
2017-02-21 23:51:39 +0000

    adds buttons to response

--- facebook-correspondance-service/index.js
+++ facebook-correspondance-service/index.js
@@ -1,16 +1,30 @@
 import settings from '../service-container/configuration';
-import {Bot, Elements} from 'facebook-messenger-bot';
+import {Bot, Elements, Buttons} from 'facebook-messenger-bot';
 
 const bot = new Bot(settings.fb.myPageToken, settings.fb.myVerification);
 
 bot.on('message', async message => {
+    let buttons;
+
     console.log(message);
     const {sender} = message;
     await sender.fetch('first_name');
 
-    const out = new Elements();
+    let out = new Elements();
     out.add({text: `Hey ${sender.first_name}, NioBi here! How are you ?`});
 
+    await bot.send(sender.id, out);
+
+    await Bot.wait(1000);
+
+    // ---- send buttons (single card)
+    buttons = new Buttons();
+    buttons.add({text: 'Google', url: 'http://google.com'});
+    buttons.add({text: 'Yahoo', url: 'http://yahoo.com'});
+    buttons.add({text: 'Bing', url: 'http://bing.com'});
+    
+    out = new Elements();
+    out.add({text: 'search engines', subtext: 'click to get redirected', buttons}); // add a card
     await bot.send(sender.id, out);
 });
 
