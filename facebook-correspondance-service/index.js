4ad8d22c359a3306abbba0451a2ba42f4a30be7a
OldFresco <hinsonkojo@gmail.com>
2017-02-22 23:33:59 +0000

    adds personality api call out

--- facebook-correspondance-service/index.js
+++ facebook-correspondance-service/index.js
@@ -1,30 +1,23 @@
 import settings from '../service-container/configuration';
-import {Bot, Elements, Buttons} from 'facebook-messenger-bot';
+import {Bot, Elements} from 'facebook-messenger-bot';
+import request from 'requisition';
 
 const bot = new Bot(settings.fb.myPageToken, settings.fb.myVerification);
 
 bot.on('message', async message => {
-    let buttons;
-
-    console.log(message);
     const {sender} = message;
+
     await sender.fetch('first_name');
 
+    let msgIn = message.message.text;
+    let smallTalkResponse = await request(`http://www.personalityforge.com/api/chat/?apiKey=eoSCUx78Dj8k0xJ2&chatBotID=3673&message=${msgIn}&externalID=1`);
+    let msgOut = smallTalkResponse.message.message;
+    let body = await msgOut.json();
+    console.log(body);
+
     let out = new Elements();
-    out.add({text: `Hey ${sender.first_name}, NioBi here! How are you ?`});
+    out.add({text: `Hey ${sender.first_name}, ${msgOut}`});
 
-    await bot.send(sender.id, out);
-
-    await Bot.wait(1000);
-
-    // ---- send buttons (single card)
-    buttons = new Buttons();
-    buttons.add({text: 'Google', url: 'http://google.com'});
-    buttons.add({text: 'Yahoo', url: 'http://yahoo.com'});
-    buttons.add({text: 'Bing', url: 'http://bing.com'});
-    
-    out = new Elements();
-    out.add({text: 'search engines', subtext: 'click to get redirected', buttons}); // add a card
     await bot.send(sender.id, out);
 });
 
