bc727b0bb008eb0ab58bc0fa08e388ce6faf4493
OldFresco <hinsonkojo@gmail.com>
2017-02-22 23:53:08 +0000

    fixes bugs

--- facebook-correspondance-service/index.js
+++ facebook-correspondance-service/index.js
@@ -15,10 +15,8 @@ bot.on('message', async message => {
     let smallTalkResponse = await request(`http://www.personalityforge.com/api/chat/?apiKey=eoSCUx78Dj8k0xJ2&chatBotID=3673&message=${msgIn}&externalID=1`);
 
     let body = await smallTalkResponse.json();
-    let msgOut = body;
+    let msgOut = body.message.message;
 
-    console.log(msgOut);
-    
     let out = new Elements();
     out.add({text: `Hey ${sender.first_name}, ${msgOut}`});
 
